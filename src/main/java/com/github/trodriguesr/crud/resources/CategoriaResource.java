45d0b9d390fdc1690b02d2a2da64e4036457e5f3
Tiago <trodriguesr@gmail.com>
2020-05-25 15:08:02 -0300

    Testando a primeira classe de dominio - Categoria

--- src/main/java/com/github/trodriguesr/crud/resources/CategoriaResource.java
+++ src/main/java/com/github/trodriguesr/crud/resources/CategoriaResource.java
@@ -1,4 +1,28 @@
 package com.github.trodriguesr.crud.resources;
 
+import com.github.trodriguesr.crud.domain.Categoria;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/categorias")
 public class CategoriaResource {
+    @RequestMapping(method = RequestMethod.GET)
+    public List<Categoria> listar(){
+
+        Categoria cat1 = new Categoria(1, "Informática");
+        Categoria cat2 = new Categoria(2,"Escritório");
+
+        List<Categoria> lista = new ArrayList<>();
+        lista.add(cat1);
+        lista.add(cat2);
+
+        return lista;
+
+    }
+
 }
